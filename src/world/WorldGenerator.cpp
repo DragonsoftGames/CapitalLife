6e5fe0d864a0c5d88e67adda5cf6c242d4617780
DasLixou <einlixou@gmail.com>
2022-10-08 16:32:03 +0200

    some worldgen tweaks

--- src/world/WorldGenerator.cpp
+++ src/world/WorldGenerator.cpp
@@ -16,8 +16,8 @@ void WorldGenerator::generateChunk(World &p_world, int p_x, int p_y)
         for (unsigned char y = 0; y < CHUNK_SIZE; y++)
         {
             auto *tile = chunk->getTileAt(x, y);
-            auto n = noise.eval(f64(p_x * CHUNK_SIZE + x) / 10.0, f64(p_y * CHUNK_SIZE + y) / 10.0);
-            if (n < 0.26)
+            auto n = noise.eval(f64(p_x * CHUNK_SIZE + x) / 23.0, f64(p_y * CHUNK_SIZE + y) / 23.0);
+            if (n < 0.21)
             {
                 tile->addBlock(new Block{BlockTypes::grass, nullptr});
             }
