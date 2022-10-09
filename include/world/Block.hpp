4e75155c718b2c34cca9c59aec8f121340ffd820
DasLixou <einlixou@gmail.com>
2022-10-09 15:25:18 +0200

    Format

--- include/world/Block.hpp
+++ include/world/Block.hpp
@@ -7,33 +7,33 @@
 class BlockData
 {
 public:
-    virtual ~BlockData() {};
+    virtual ~BlockData(){};
 };
 
 class BlockRenderer
 {
 public:
-    virtual ~BlockRenderer() {};
+    virtual ~BlockRenderer(){};
 
-    virtual void render(Camera& p_camera, int p_x, int p_y) = 0;
+    virtual void render(Camera &p_camera, int p_x, int p_y) = 0;
 };
 
 struct BlockType
 {
     ~BlockType();
 
-    const char* id;
+    const char *id;
     bool isTranparent;
-    BlockRenderer* renderer;
+    BlockRenderer *renderer;
 };
 
 struct Block
 {
     ~Block();
 
-    BlockType* type;
-    BlockData* data;
-    inline void render(Camera& p_camera, int p_x, int p_y)
+    BlockType *type;
+    BlockData *data;
+    inline void render(Camera &p_camera, int p_x, int p_y)
     {
         type->renderer->render(p_camera, p_x, p_y);
     }
--- include/world/block/BlockRenderers.hpp
+++ include/world/block/BlockRenderers.hpp
@@ -6,16 +6,17 @@
 class StaticBlockRenderer : public BlockRenderer
 {
 public:
-    StaticBlockRenderer(SDL_Texture* p_tex) :texture(p_tex) {}
+    StaticBlockRenderer(SDL_Texture *p_tex) : texture(p_tex) {}
     ~StaticBlockRenderer()
     {
         SDL_DestroyTexture(texture);
     }
 
-    void render(Camera& p_camera, int p_x, int p_y)
+    void render(Camera &p_camera, int p_x, int p_y)
     {
-        p_camera.render(texture, {0, 0, 32, 32}, Vec2{DEFAULT_BLOCK_SIZE*p_x, DEFAULT_BLOCK_SIZE*p_y}, Vec2{DEFAULT_BLOCK_SIZE, DEFAULT_BLOCK_SIZE});
+        p_camera.render(texture, {0, 0, 32, 32}, Vec2{DEFAULT_BLOCK_SIZE * p_x, DEFAULT_BLOCK_SIZE * p_y}, Vec2{DEFAULT_BLOCK_SIZE, DEFAULT_BLOCK_SIZE});
     }
+
 private:
-    SDL_Texture* texture;
+    SDL_Texture *texture;
 };
